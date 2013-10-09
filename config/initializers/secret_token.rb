9b7feb56dfcb8ab52c433325f80d7c342155c1b3
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-09 16:03:47 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DbcPower::Application.config.secret_key_base = '5074383a61c7f07290875a519b2f4c1e193d08cdb26da656a1d14ed44b115c60f003651998e2aaed85020c76aeea0aed9ffcd63ef19b9019c27e943a84aa8e57'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DbcPower::Application.config.session_store :cookie_store, key: '_dbc_power_session'
