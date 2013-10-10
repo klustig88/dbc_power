35d06defa50b9be22a24db31cf6a919302b90cc3
richard bluestein <richard@bluestein.com>
2013-10-10 11:43:11 -0500

    oauth ssl error

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,6 +1,11 @@
 class SessionsController < ApplicationController
   def create
+  	P sessin[:user_id	]
     user = User.from_omniauth(env["omniauth.auth"])
+    p "XXXXXXCCCCCCFCFCCFCCCCCCCCC"
+    p user
+		p "XXXXXXCCCCCCFCFCCFCCCCCCCCC"
+
     session[:user_id] = user.id
     redirect_to root_url
   end
