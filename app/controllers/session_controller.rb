33b70d2e53d7d59d87cb3c0c53d9644e7865f767
richard bluestein <richard@bluestein.com>
2013-10-09 21:31:26 -0500

    adding oauth params

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def create
+<<<<<<< HEAD
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
     redirect_to root_url
@@ -8,5 +9,16 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to root_url
+=======
+    @user = User.find_or_create_from_auth_hash(auth_hash)
+    self.current_user = @user
+    redirect_to '/'
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+>>>>>>> adding oauth params
   end
 end
