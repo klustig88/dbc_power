32c4e48bd3a7e66715210dc59f1a5b534f84a6ab
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-09 21:13:16 -0500

    finished_test2 for user model

--- spec/models/user_model_spec.rb
+++ spec/models/user_model_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
 
 
+
 describe User do
+
+  let(:user) { User.new( ) }
+
   it "should not save if username is not there" do
-    User.create().should eq(nil)     
+    user.save.should eq(false)     
   end
+
+  it "should start with a default value of 3" do
+    @user= User.create(username: "hannahbarbarah", password: "password", password_confirmation: "password", email: "s@ma.com" )
+    @user.votes_left.should eq(2)
+  end  
 end    
