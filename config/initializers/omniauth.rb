fb401491946deffa2f6da646e4e670c3a3046947
richard bluestein <richard@bluestein.com>
2013-10-10 13:30:33 -0500

    oauth working

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :dbc, ENV['OAUTH_CLIENT_ID'], ENV['OAUTH_CLIENT_SECRET'], {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
+  provider :dbc, ENV['OAUTH_CLIENT_ID'], ENV['OAUTH_CLIENT_SECRET']#, {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
 end
  
