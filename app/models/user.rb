9b92c903e53c246e69abebd1c2d68f1fcd4dc6b1
richard bluestein <richard@bluestein.com>
2013-10-10 17:15:17 -0500

    cleaning user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,7 @@
 class User < ActiveRecord::Base
   before_save :default_values
   before_save :stock_votes
-
-  validates :email, uniqueness: true, format: /.+@.+\..+/
-
-  validates :username, uniqueness:true, presence:true
-
   has_many :comments
-  # has_secure_password
 
   def default_values
     self.current_score ||= 0
