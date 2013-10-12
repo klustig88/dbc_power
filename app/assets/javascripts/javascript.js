060bcd710e49bbf49220043aa42b9cc18ad0daf8
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-12 16:21:07 -0500

    js twerking all out should be bug free

--- app/assets/javascripts/javascript.js
+++ app/assets/javascripts/javascript.js
@@ -21,14 +21,26 @@
   // });
 // });
 
-$(function() {
-  initPage();
-});
-$(window).bind('page:change', function() {
-  initPage();
-});
-function initPage() {
-    $('#student').hide();
+
+
+
+
+function responder(response){
+  if(response == "Make it bigger!"){
+    $('.comment').hide();         
+    $('ul').append("<li class=\"bigger\"  >"+ response  +"</li>");
+    }  
+  else if(response == "Dude buy some votes!"){  
+    $('.comment').hide();  
+    $('#student').hide();     
+    $('.info').append("<li class=\"bigger\"  >"+ response  +"</li>");
+    }else{
+      $('.comment').prepend("<li class=\"addition\"  >"+ response  +"</li>");
+  }  
+}
+
+function poster(){
+$('#student').hide();
     $("#up_vote_button").on('click', function(e){
     e.preventDefault();
     $('#student').show();
@@ -40,22 +52,28 @@ function initPage() {
       var data = $(this).serialize();
       console.log(data)
       $.post(url,data,function(response){
-        if(response.comment == "Make it bigger!"){
-          $('.comment').hide();         
-          $('ul').append("<li class=\"bigger\"  >"+ response.comment  +"</li>");
-        }  
-        else if(response.comment == "Dude buy some votes!"){  
-          $('.comment').hide();  
-          $('#student').hide();     
-          $('ul').append("<li class=\"bigger\"  >"+ response.comment  +"</li>");
-        }else{
-          $('.comment').append("<li class=\"addition\"  >"+ response.comment  +"</li>");
-        }  
+        responder(response.comment);
       });
-
-    
-
     });
   });
+}
+
+
+
+
+
+
+
+
+  
+
+$(function() {
+  initPage();
+});
+$(window).bind('page:change', function() {
+  initPage();
+});
+function initPage() {
+    poster();
 
 }
