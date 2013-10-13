2f7e637ebb56ede92ae1357f8726277acc8d22ad
dbc-apprentice <apprentice@dbc-chicago-008.local>
2013-10-13 17:31:41 -0500

    fixed sliding loading issues

--- app/assets/javascripts/sliding.js
+++ app/assets/javascripts/sliding.js
@@ -1,4 +1,4 @@
-$(function() {
+function menu_slide() {
   $('#open_settings').on('click', function() {
     $('#settings').animate({right: '0'}, 1000, function() {
 
@@ -10,4 +10,29 @@ $(function() {
 
     });
   });
+}
+
+
+// $(function() {
+//   $('#open_settings').on('click', function() {
+//     $('#settings').animate({right: '0'}, 1000, function() {
+
+//     });
+//   });
+
+//   $('#close_settings').on('click', function() {
+//     $('#settings').animate({right: '-300'}, 1000, function() {
+
+//     });
+//   });
+// });
+
+$(function() {
+  initPage();
 });
+$(window).bind('page:change', function() {
+  initPage();
+});
+function initPage() {
+    menu_slide();
+}
